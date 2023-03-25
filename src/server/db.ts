7aa54c056bf3944557fae93412513ad7b4d27b93
Raihan Adam <hallwack.id@gmail.com>
2023-03-25 17:34:40 +0700

    feat: add hashing in prisma db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "@/env.mjs";
+import argon2 from "argon2";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
@@ -11,4 +12,35 @@ export const prisma =
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (env.NODE_ENV !== "production") {
+  prisma.$use(async (params, next) => {
+    if (params.model === "User" && params.action === "create")
+      params.args.data.password = await argon2.hash(params.args.data.password);
+    return next(params);
+  });
+
+  prisma.$use(async (params, next) => {
+    if (params.model === "User" && params.action === "findFirst") {
+      const password = params.args.where?.password;
+      if (password) delete params.args.where.password;
+
+      const user = await next(params);
+
+      if (password) {
+        try {
+          if (user && (await argon2.verify(user.password, password)))
+            return user;
+        } catch (error) {
+          return null;
+        }
+        return null;
+      } else {
+        return user;
+      }
+    }
+
+    return next(params);
+  });
+
+  globalForPrisma.prisma = prisma;
+}
